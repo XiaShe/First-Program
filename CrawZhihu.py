0d0195900ffd351a1d52148ee44e6e99e18b0c5b
XIA <53223311+XiaShe@users.noreply.github.com>
2019-08-02 12:41:05 +0800

    改进代码
    增加了显示爬取到具体页面的功能，修改了进度百分比不准确的问题

--- CrawZhihu.py
+++ CrawZhihu.py
@@ -7,7 +7,7 @@ import requests
 import os
 import re
 
-def get_Text(url):
+def get_Text(url):#获取html界面
     # user-agent 与 cookie: : 右键检查或者F12 Network - all 下拉
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
@@ -20,47 +20,50 @@ def get_Text(url):
     except:
         return ''
 
-def get_Pictures(Purl):
+def get_Pictures(Purl):#获取图片链接并下载到本地
+    count = 0
     html = get_Text(Purl)
     urls = re.findall(r'\"name\":\"\w*\"|https://pic\d.zhimg.com/v2-\w*r.jpg', html)#正则表达式
     scale = len(urls)
-    count = 0
     for i in range(scale):
+        count = count + 1
+        print("\r当前页图片爬取进度: {:.2f}%".format(count * 100 / scale), end="")
         url = urls[i]#列表->字符串
-        if url.startswith("\"name\""):
+        if url.startswith("\"name\""):#挑选出开头为"name"的字符串，赋值于name
             name = url
         else:
-            #print(name)
-            #print(url)
             root = "D://pics//"
             path = root + name.replace('\"', '').replace(':','').replace('name','') + "  " + url.split('/')[-1]
             # 答主名字+链接名字作为图片名字（链接名称为了去重），去掉多余字符
             try:
                 if not os.path.exists(root):  # 判断文件夹是否存在
                     os.mkdir(root)  # 创建文件夹
-                if not os.path.exists(path):  # 判断图片是否存在,由于本程序爬取知乎为异步加载，因此产生许多重复链接
+                if not os.path.exists(path):
+                    # 判断图片是否存在,由于本程序爬取知乎为异步加载，因此产生许多重复链接，元素去重用集合也可，不过会打乱顺序，不适合本程序
                     r = requests.get(url)
                     with open(path, 'wb') as f:
                         f.write(r.content)
                         # 返回二进制型数据，一般取图片文件使用，取文本使用r.text
                         f.close()
                         #print("文件保存成功")
-                        count = count + 1
-                        print("\r当前图片爬取进度: {:.2f}%".format(count * 100 / scale), end="")
                 #else:
                     #print("文件已存在")
                 #由于获取页面采用正则表达式爬取，有两个完全相同的url链接，故第二个忽略
             except:
                 #print("爬取失败")
-                print("\r当前进度: {:.2f}%".format(count * 100 / scale), end="")
+                print("\r当前页图片爬取进度: {:.2f}%".format(count * 100 /scale), end="")
                 continue
 
 def main():
     number = 0
-    while (number < 5):#爬取“页数”
+    pages = 1
+    while (number <= 20):#爬取“页数”
         url = "https://www.zhihu.com/api/v4/questions/333026642/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset=" \
               + str(number) + "&platform=desktop&sort_by=default"
+        print("正在爬取第{}页".format(pages))
         get_Pictures(url)
+        print("\n")
+        pages += 1
         number += 5
 
 #分析 Request URL
@@ -70,5 +73,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
