71ced219bc46583a9e7c467c120c91223f8b8c75
XIA <53223311+XiaShe@users.noreply.github.com>
2019-08-26 16:28:35 +0800

    添加注释

--- CrawZhihu.py
+++ CrawZhihu.py
@@ -3,6 +3,7 @@
 #正则表达式
 #实时显示爬取进度
 #知乎界面为异步加载，本程序截获浏览器发给服务器的请求，分析出请求规律，伪装成浏览器不断向服务器发送请求
+#如果第一个问题为视频，会弹出错误，更改页数即可
 import requests
 import os
 import re
@@ -60,6 +61,8 @@ def main():
     while (number <= 20):#爬取“页数”
         url = "https://www.zhihu.com/api/v4/questions/333026642/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset=" \
               + str(number) + "&platform=desktop&sort_by=default"
+#         url = "https://www.zhihu.com/api/v4/questions/299205851/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset= " \
+#               + str(number) + "&platform=desktop&sort_by=default" # 问题《有什么美得不可方物的壁纸？》
         print("正在爬取第{}页".format(pages))
         get_Pictures(url)
         print("\n")
