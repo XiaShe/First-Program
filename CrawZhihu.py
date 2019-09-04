e90bfc0a6d80637ef2c1e4f2fb043c029b16b6e5
XIA <53223311+XiaShe@users.noreply.github.com>
2019-09-04 17:26:08 +0800

    加速爬虫

--- CrawZhihu.py
+++ CrawZhihu.py
@@ -3,7 +3,6 @@
 #正则表达式
 #实时显示爬取进度
 #知乎界面为异步加载，本程序截获浏览器发给服务器的请求，分析出请求规律，伪装成浏览器不断向服务器发送请求
-#如果第一个问题为视频，会弹出错误，更改页数即可
 import requests
 import os
 import re
@@ -14,9 +13,10 @@ def get_Text(url):#获取html界面
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
     try:
         r = requests.get(url, headers=headers, timeout=30)
-        # r = requests.get(url, cookies=cookies, headers=headers, timeout=30)
+        # r = requests.get(url, cookies=cookies, headers=headers, t返回path指定的文件夹包含的文件或文件夹的名字的列表。imeout=30)
         r.raise_for_status()
-        r.encoding = r.apparent_encoding  # 可修改，加速
+        r.encoding = "utf-8" #直接给定爬取网页编码格式
+        #r.encoding = r.apparent_encoding  # 可修改，加速
         return r.text
     except:
         return ''
@@ -26,6 +26,7 @@ def get_Pictures(Purl):#获取图片链接并下载到本地
     html = get_Text(Purl)
     urls = re.findall(r'\"name\":\"\w*\"|https://pic\d.zhimg.com/v2-\w*r.jpg', html)#正则表达式
     scale = len(urls)
+    #print(urls)
     for i in range(scale):
         count = count + 1
         print("\r当前页图片爬取进度: {:.2f}%".format(count * 100 / scale), end="")
@@ -57,12 +58,10 @@ def get_Pictures(Purl):#获取图片链接并下载到本地
 
 def main():
     number = 0
-    pages = 1
-    while (number <= 20):#爬取“页数”
+    pages = 0
+    while (number <= 50):#爬取“页数”
         url = "https://www.zhihu.com/api/v4/questions/333026642/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset=" \
               + str(number) + "&platform=desktop&sort_by=default"
-#         url = "https://www.zhihu.com/api/v4/questions/299205851/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_labeled%2Cis_recognized%2Cpaid_info%2Cpaid_info_content%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cbadge%5B%2A%5D.topics&limit=5&offset= " \
-#               + str(number) + "&platform=desktop&sort_by=default" # 问题《有什么美得不可方物的壁纸？》
         print("正在爬取第{}页".format(pages))
         get_Pictures(url)
         print("\n")
